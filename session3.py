34ae42529ac5f686c2b78caaba777bf3bfe4e0b8
malikpajaranugrah <malik.pajar_ti22@nusaputra.ac.id>
2022-10-07 14:30:27 +0700

    session3

---
+++ session3.py
@@ -0,0 +1,11 @@
+name = "Malik Pajar Anugrah"
+Class = "TI22I"
+major = "Informatic Engineering"
+quote = "don't limit your steps"
+age = 19
+ilikespicefood = True
+hobbies = ["sports","watch movie","travelling"]
+
+print(f"My name is: {name}\nClass: {Class}\nMajor: {major}\nQuote: {quote}\nAge: {age}\n"
+      f"I Like Spice Food: {ilikespicefood}\nHobbies: {hobbies}")
+
